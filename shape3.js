b77843f630621d4d8d2f130a4fe04df6d86329d5
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-02 18:05:31 +0200

    One of the best shapes

---
+++ shape3.js
@@ -0,0 +1,25 @@
+//Mathematical Rose Patterns
+let d = 8;
+let n = 5;
+
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  let k = n/d;
+  background(255);
+  translate(width/2, height/2);
+  noFill();
+  
+  beginShape();
+  for ( let a = 0; a < TWO_PI * d; a += 0.002){
+    let r = 200 * cos(k * a);
+    let x = r * cos(a);
+    let y = r * sin(a);
+    stroke(0);
+    strokeWeight(4);
+    vertex(x, y);
+  }
+  endShape(CLOSE);
+}
